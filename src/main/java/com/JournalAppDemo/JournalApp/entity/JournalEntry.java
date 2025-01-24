d5da7ec39c16b2f1316503fe52523284228c5129
smitkachhadiya <114665787+smitkachhadiya@users.noreply.github.com>
2025-01-24 17:18:02 +0530

    Adding Lombok Dependency

--- src/main/java/com/JournalAppDemo/JournalApp/entity/JournalEntry.java
+++ src/main/java/com/JournalAppDemo/JournalApp/entity/JournalEntry.java
@@ -2,6 +2,7 @@ package com.JournalAppDemo.JournalApp.entity;
 
 // Creating A POJO (Plain Old Java Object) class
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
@@ -18,37 +20,37 @@ public class JournalEntry {
     private String title;
     private String content;
     private LocalDateTime date;
-
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
+//
+//
+//    public ObjectId getId() {
+//        return id;
+//    }
+//
+//    public void setId(ObjectId id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public LocalDateTime getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDateTime date) {
+//        this.date = date;
+//    }
 }
