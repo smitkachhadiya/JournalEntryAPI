725806dd79a0052d3056404d6127518be8178160
smitkachhadiya <114665787+smitkachhadiya@users.noreply.github.com>
2025-05-24 19:55:11 +0530

    Adding profiles (dev , prod) and converting properties to yml format

--- src/main/java/com/JournalAppDemo/JournalApp/config/SpringSecurity.java
+++ src/main/java/com/JournalAppDemo/JournalApp/config/SpringSecurity.java
@@ -4,16 +4,19 @@ import com.JournalAppDemo.JournalApp.service.UserDetailServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Profile("dev")
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
